2588a0e941aabdf31ecff2e40ec6995b68a62082
nguyendangtu <nguyendangtu1987@gmail.com>
2018-09-23 19:08:05 +0800

    init

--- src/main/java/hashCode_Equal/Key.java
+++ src/main/java/hashCode_Equal/Key.java
@@ -4,15 +4,15 @@ import java.util.HashMap;
 
 /**
  * I. This class is an example how implement hashCode and Equal method. HashMap contains an array of Node and Node can be can represent
- * as class following objects. 1. hash (int), 2. Key (K), 3. Value (V), 4. Node next.
- * II. When we put a value to a hash map, hashCode function will get the hash of object and put the object to a node.
- * All node the same hash will store on a linked set. In case of Collision, we will put the object the same hash code on the same set.
- * If value the same, the object will be override, If value is difference, hash map will add the new object after existing object into the set.
- * When we get a value from a hash map, we will find the linked set base on the hash code of the key, they we will compare with the first object
- * in the set, if not equal, we will compare to the next node and go to util we find a object into the linked set.
+ *     as class following objects. 1. hash (int), 2. Key (K), 3. Value (V), 4. Node next.
+ * II. When we put a value to a hash map, hashCode function will get the hash code value of object and put the object to a node.
+ *      all item the same hash will store on a linked set. In case of Collision, we will put the object the same hash code on the same set.
+ *      If value the same, the object will be override, If value is difference, hash map will add the new object after existing object into the set.
+ *     When we get a value from a hash map, HashMap will find the linked set base on the hash code of the key, they will compare with the first object
+ *       in the set, if not equal, we will compare to the next item and continue util HashMap find out an object into the linked set.
  * III. If we override hashcode, we can find the value when we create a new instance with the same input on constructor.
- * If we not override hash code, we can not find the value when we create a new instance with the same input on constructor because two new instance will be
- * difference on the hash code value
+ *      If we not override hash code, we can not find the value when we create a new instance with the same input on constructor because two new instance will be
+ *        difference on the hash code value.
  */
 public class Key {
     String key;
