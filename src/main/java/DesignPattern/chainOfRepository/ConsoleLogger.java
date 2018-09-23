9c8d4d9d92787a43f0d5a2a45ac8017564167887
nguyendangtu <nguyendangtu1987@gmail.com>
2018-09-24 01:20:13 +0800

    Chain Of Repository Pattern

---
+++ src/main/java/DesignPattern/chainOfRepository/ConsoleLogger.java
@@ -0,0 +1,13 @@
+package DesignPattern.chainOfRepository;
+
+public class ConsoleLogger extends AbstractLogger {
+
+    public ConsoleLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    void write(String message) {
+        System.out.println("Console::Logger:" + message);
+    }
+}
---
+++ src/main/java/DesignPattern/chainOfRepository/FileLogger.java
@@ -0,0 +1,12 @@
+package DesignPattern.chainOfRepository;
+
+public class FileLogger extends AbstractLogger {
+    public FileLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    void write(String message) {
+        System.out.println("File::Logger:" + message);
+    }
+}
