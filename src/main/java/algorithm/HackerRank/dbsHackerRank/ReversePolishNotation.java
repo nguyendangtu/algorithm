e4698daea6fb5d5be5ff9c117c19871e34e43e7e
JOHNNGUYEN <nguyendangtu1987@gmail.com>
2019-02-20 22:05:46 +0800

    reserve polish notation by using stack

---
+++ src/main/java/algorithm/HackerRank/dbsHackerRank/ReversePolishNotation.java
@@ -0,0 +1,41 @@
+package algorithm.HackerRank.dbsHackerRank;
+
+import java.util.Stack;
+
+public class ReversePolishNotation {
+    public static void main(String args[]) {
+        String[] tokens = new String[]{"2", "1", "+", "3", "*"};
+        System.out.println(regularExpression(tokens));
+    }
+
+    public static int regularExpression(String[] tokens) {
+
+        int returnValue = 0;
+        String operators = "+-*/";
+        Stack<String> stack = new Stack<String>();
+        for (String t : tokens) {
+            if (!operators.contains(t)) { //push to stack if it is a number
+                stack.push(t);
+            } else {//pop numbers from stack if it is an operator
+                int a = Integer.valueOf(stack.pop());
+                int b = Integer.valueOf(stack.pop());
+                switch (t) {
+                    case "+":
+                        stack.push(String.valueOf(a + b));
+                        break;
+                    case "-":
+                        stack.push(String.valueOf(b - a));
+                        break;
+                    case "*":
+                        stack.push(String.valueOf(a * b));
+                        break;
+                    case "/":
+                        stack.push(String.valueOf(b / a));
+                        break;
+                }
+            }
+        }
+        returnValue = Integer.valueOf(stack.pop());
+        return returnValue;
+    }
+}
